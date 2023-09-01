5e424a271c05c4c15251e625021d38552df2b09c
Carlos Franzreb <>
2023-09-01 11:13:51 +0200

    Fine-tune spkemb models with speechbrain

---
+++ spkanon_eval/featex/spkid/finetune.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+"""Recipe for training speaker embeddings (e.g, xvectors) using the VoxCeleb Dataset.
+We employ an encoder followed by a speaker classifier.
+
+To run this recipe, use the following command:
+> python train_speaker_embeddings.py {hyperparameter_file}
+
+Using your own hyperparameter file or one of the following:
+    hyperparams/train_x_vectors.yaml (for standard xvectors)
+    hyperparams/train_ecapa_tdnn.yaml (for the ecapa+tdnn system)
+
+Author
+    * Mirco Ravanelli 2020
+    * Hwidong Na 2020
+    * Nauman Dawalatabad 2020
+"""
+
+
+import random
+import torch
+import torchaudio
+import speechbrain as sb
+from speechbrain.dataio.dataset import DynamicItemDataset
+
+
+class SpeakerBrain(sb.core.Brain):
+    """Class for speaker embedding training"""
+
+    def compute_forward(self, batch, stage):
+        """Computation pipeline based on a encoder + speaker classifier.
+        Data augmentation and environmental corruption are applied to the
+        input speech.
+        """
+        batch = batch.to(self.device)
+        wavs, lens = batch.sig
+
+        if stage == sb.Stage.TRAIN:
+            wavs_aug_tot = []
+            wavs_aug_tot.append(wavs)
+            for augment in self.hparams.augment_pipeline:
+                wavs_aug = augment(wavs, lens)
+
+                if wavs_aug.shape[1] > wavs.shape[1]:
+                    wavs_aug = wavs_aug[:, 0 : wavs.shape[1]]
+                else:
+                    zero_sig = torch.zeros_like(wavs)
+                    zero_sig[:, 0 : wavs_aug.shape[1]] = wavs_aug
+                    wavs_aug = zero_sig
+
+                if self.hparams.concat_augment:
+                    wavs_aug_tot.append(wavs_aug)
+                else:
+                    wavs = wavs_aug
+                    wavs_aug_tot[0] = wavs
+
+            wavs = torch.cat(wavs_aug_tot, dim=0)
+            self.n_augment = len(wavs_aug_tot)
+            lens = torch.cat([lens] * self.n_augment)
+
+        feats = self.modules.compute_features(wavs)
+        feats = self.modules.mean_var_norm(feats, lens)
+        embeddings = self.modules.embedding_model(feats)
+        outputs = self.modules.classifier(embeddings)
+
+        return outputs, lens
+
+    def compute_objectives(self, predictions, batch, stage):
+        """Computes the loss using speaker-id as label."""
+        predictions, lens = predictions
+        uttid = batch.id
+        spkid = torch.tensor([int(spkid) for spkid in batch.spk_id_encoded]).unsqueeze(
+            1
+        )
+
+        if stage == sb.Stage.TRAIN:
+            spkid = torch.cat([spkid] * self.n_augment, dim=0)
+
+        loss = self.hparams.compute_cost(predictions, spkid, lens)
+
+        if stage == sb.Stage.TRAIN and hasattr(
+            self.hparams.lr_annealing, "on_batch_end"
+        ):
+            self.hparams.lr_annealing.on_batch_end(self.optimizer)
+
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics.append(uttid, predictions, spkid, lens)
+
+        return loss
+
+    def on_stage_start(self, stage, epoch=None):
+        """Gets called at the beginning of an epoch."""
+        if stage != sb.Stage.TRAIN:
+            self.error_metrics = self.hparams.error_stats()
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        """Gets called at the end of an epoch."""
+        # Compute/store important stats
+        stage_stats = {"loss": stage_loss}
+        if stage == sb.Stage.TRAIN:
+            self.train_stats = stage_stats
+        else:
+            stage_stats["ErrorRate"] = self.error_metrics.summarize("average")
+
+        # Perform end-of-iteration things, like annealing, logging, etc.
+        if stage == sb.Stage.VALID:
+            old_lr, new_lr = self.hparams.lr_annealing(epoch)
+            sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
+
+            self.hparams.train_logger.log_stats(
+                stats_meta={"epoch": epoch, "lr": old_lr},
+                train_stats=self.train_stats,
+                valid_stats=stage_stats,
+            )
+
+
+def prepare_dataset(hparams: dict, train_datafile: str) -> DynamicItemDataset:
+    "Creates the datasets and their data processing pipelines."
+
+    snt_len_sample = int(hparams["sample_rate"] * hparams["sentence_len"])
+    train_data = DynamicItemDataset.from_csv(
+        csv_path=train_datafile,
+    )
+    datasets = [train_data]
+
+    # Define audio pipeline:
+    @sb.utils.data_pipeline.takes("wav", "duration")
+    @sb.utils.data_pipeline.provides("sig")
+    def audio_pipeline(wav, duration):
+        duration_sample = int(duration * hparams["sample_rate"])
+        if hparams["random_chunk"]:
+            start = random.randint(0, duration_sample - snt_len_sample)
+            stop = start + snt_len_sample
+        else:
+            start = 0
+            stop = duration_sample
+        num_frames = stop - start
+        sig, fs = torchaudio.load(wav, num_frames=num_frames, frame_offset=start)
+        sig = sig.transpose(0, 1).squeeze(1)
+        return sig
+
+    sb.dataio.dataset.add_dynamic_item(datasets, audio_pipeline)
+    sb.dataio.dataset.set_output_keys(datasets, ["id", "sig", "spk_id_encoded"])
+
+    return train_data
--- spkanon_eval/featex/spkid/spkid.py
+++ spkanon_eval/featex/spkid/spkid.py
@@ -6,8 +6,16 @@ embeddings.
 
 import os
 import logging
+import json
+import csv
 
 from speechbrain.pretrained import EncoderClassifier
+from speechbrain.utils.checkpoints import Checkpointer
+from hyperpyyaml import load_hyperpyyaml
+from omegaconf import OmegaConf
+import torch
+
+from spkanon_eval.featex.spkid.finetune import SpeakerBrain, prepare_dataset
 
 
 LOGGER = logging.getLogger("progress")
@@ -15,7 +23,7 @@ SAMPLE_RATE = 16000
 
 
 class SpkId:
-    def __init__(self, config, device):
+    def __init__(self, config: OmegaConf, device: str) -> None:
         """Initialize the model with the given config and freeze its parameters."""
         self.config = config
         self.device = device
@@ -27,8 +35,83 @@ class SpkId:
         )
         self.model.eval()
 
-    def run(self, batch):
+    def run(self, batch: list[torch.Tensor]) -> torch.Tensor:
+        """
+        Return speaker embeddings for the given batch of utterances.
+
+        Args:
+            batch: A list of two tensors, the first containing the waveforms
+            with shape (batch_size, n_samples), and the second containing
+            the speaker labels as integers with shape (batch_size).
+
+        Returns:
+            A tensor containing the speaker embeddings with shape
+            (batch_size, embedding_dim).
+        """
         return self.model.encode_batch(batch[0].to(self.device)).squeeze(1)
 
-    def finetune(self, exp_folder, datafiles, n_speakers):
-        return NotImplementedError
+    def finetune(self, dump_dir: str, datafiles: list[str], n_speakers: int) -> None:
+        """
+        Fine-tune this model with the given datafiles.
+
+        Args:
+            dump_dir: Path to the folder where the model and datafiles will be saved.
+            datafiles: List of paths to the datafiles used for fine-tuning.
+            n_speakers: Number of speakers across all datafiles, used to initialize
+                the classifier.
+        """
+
+        # create the train datafile as expected by SpeechBrain
+        os.makedirs(dump_dir, exist_ok=True)
+        train_datafile = os.path.join(dump_dir, "train_datafile.json")
+        csv_file = open(train_datafile, "w")
+        csv_writer = csv.writer(
+            csv_file,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL,
+        )
+        speaker_ids = list()
+        csv_writer.writerow(["ID", "duration", "wav", "spk_id", "spk_id_encoded"])
+        for datafile in datafiles:
+            for line in open(datafile):
+                obj = json.loads(line)
+                if obj["label"] not in speaker_ids:
+                    speaker_ids.append(obj["label"])
+                csv_writer.writerow(
+                    [
+                        obj["audio_filepath"],
+                        obj["duration"],
+                        obj["audio_filepath"],
+                        obj["label"],
+                        speaker_ids.index(obj["label"]),
+                    ]
+                )
+        csv_file.close()
+        json.dump(speaker_ids, open(os.path.join(dump_dir, "spk_ids.json"), "w"))
+
+        # train the model
+        with open(self.config.finetune_config) as f:
+            hparams = load_hyperpyyaml(f, overrides={"output_folder": dump_dir})
+        train_data = prepare_dataset(hparams, train_datafile)
+        hparams["out_n_neurons"] = n_speakers
+        speaker_brain = SpeakerBrain(
+            modules=hparams["modules"],
+            opt_class=hparams["opt_class"],
+            hparams=hparams,
+        )
+        speaker_brain.fit(
+            speaker_brain.hparams.epoch_counter,
+            train_data,
+            train_loader_kwargs=hparams["dataloader_options"],
+        )
+
+        # save the embedding model and load it
+        checkpointer = Checkpointer(
+            dump_dir,
+            recoverables={
+                "embedding_model": speaker_brain.modules.embedding_model,
+                "classifier": speaker_brain.modules.classifier,
+            },
+        )
+        checkpointer.save_checkpoint(name="spkid_model")
