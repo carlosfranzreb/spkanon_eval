8774aac03c2392e1ef391dc30b1448227877eb33
Carlos (HPC) <carlos.franzreb@dfki.de>
2024-06-03 16:53:43 +0200

    delete LOCAL_RANK env variable

--- spkanon_eval/main.py
+++ spkanon_eval/main.py
@@ -16,6 +16,9 @@ LOGGER = logging.getLogger("progress")
 def main(config: OmegaConf, exp_folder: str):
     """Run the jobs (training, inference, evaluation) as specified in the config."""
 
+    # Otherwise SB assumes we are running a distributed job
+    del os.environ["LOCAL_RANK"]
+
     target_df = config.data.datasets.get("targets", list())
     if len(target_df) > 0:
         prepare_datafile("targets", config, exp_folder)
